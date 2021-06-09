62d87d56c902426f6a882b322abe03377fb6a7d7
Velerium <bondaniandrea@libero.it>
2021-06-09 16:28:16 +0200

    Added "Pari e dispari". Project over! Might have to clean up a bit, but it'll do for now.

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ function again() {  // Because why not?
 
     if (!word || word.length === 0) {
     alert('Invalid value.');
+    userWord.innerHTML = '';
     } else {
         userWord.innerHTML = 'The word ' + '<span id="underline">' + word + '</span>' + ':';
     }
@@ -42,3 +43,43 @@ again();
 
 // ----------- PARI O DISPARI? ----------- //
 
+var short2 = document.getElementById('output-player');
+var short3 = document.getElementById('output-cpu');
+var choices = document.getElementsByClassName('choices');
+var winner = document.getElementById('winner');
+
+function numberGame() { //Triggered only by button.
+
+    if (choices[0].checked === false && choices[1].checked === false) {
+        winner.innerHTML = 'Please select a choice!'
+        return;
+    }
+
+    var playerNumber = parseInt(prompt('Pick a number from 1 to 5!'));
+
+    if (playerNumber < 1 || playerNumber > 5) {
+        winner.innerHTML = 'Please pick a number between 1 and 5.'
+        return;
+    }
+
+    short2.innerHTML = playerNumber;
+
+    var cpuNumber = Math.floor(Math.random() * 5) + 1;
+    short3.innerHTML = cpuNumber;
+
+    var total = playerNumber + cpuNumber;
+
+    if (choices[0].checked === true) { // EVEN
+        if (total % 2 === 0) {
+            winner.innerHTML = 'You win!!'
+        } else {
+            winner.innerHTML = 'You lose...'
+        }
+    } else { // ODD
+        if (total % 2 === 1) {
+            winner.innerHTML = 'You win!!'
+        } else {
+            winner.innerHTML = 'You lose...'
+        }
+    }
+}
